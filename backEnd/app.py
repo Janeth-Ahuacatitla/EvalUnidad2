afdedd6e502a795fc5c60fc0ee1e68babd4f35e4
Janeth <210084@utxicotepec.edu.mx>
2024-07-04 21:30:17 -0600

    agregar carpetas y users.py

--- backEnd/app.py
+++ backEnd/app.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from routes.persona import persona
 from routes.usuarios import usuario
+from routes.users import user
+
 
 app = FastAPI()
 app.include_router(persona)
 app.include_router(usuario)
+app.include_router(user)
 
 print ("Bienvenido a mi aplicación")
