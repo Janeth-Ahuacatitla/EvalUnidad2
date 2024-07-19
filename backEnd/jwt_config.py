7ca98e91f11b4e71450c294bc096965d448ffdd8
Janeth <210084@utxicotepec.edu.mx>
2024-07-19 13:42:06 -0600

    modificar correo

--- backEnd/jwt_config.py
+++ backEnd/jwt_config.py
@@ -1,8 +1,8 @@
 from jwt import encode, decode
 
 def solicita_token (dato:dict)->str:
-    token:str=encode(payload=dato,key='210084',algorithm='HS256')
+    token:str=encode(payload=dato,key='tu_clave_secreta',algorithm='HS256')
     return token
 def valida_token(token:str)->dict:
-    dato:dict = decode(token,key='210084',algorithms=['HS256'])
+    dato:dict = decode(token,key='tu_clave_secreta',algorithms=['HS256'])
     return dato
